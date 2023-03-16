83d3f67bd55194aa0dc801e474e14a8cb4f2041f
Antonio Iatonna <ant.iatonna@gmail.com>
2023-03-16 12:11:33 -0400

    Added Instructions for Making Edits
    Ensure everyone is on the same page

--- codes280/RoomObjects.java
+++ codes280/RoomObjects.java
@@ -1,6 +1,8 @@
+/* Change the package to the location where you are running the code (same folder as labs and assignments).
+Make your changes in that folder and once you have tested and are ready to commit changes, copy and paste the files into
+the repository folder, replacing the old ones. Please make sure previously working code is not broken before you commit changes
+*/
 package FinalProject.ModellingThe3DWorld.codes280;
-
-// package codes280;
 
 // This is where our objects will go:
 
--- codes280/RoomSimulator.java
+++ codes280/RoomSimulator.java
@@ -1,5 +1,8 @@
+/* Change the package to the location where you are running the code (same folder as labs and assignments).
+Make your changes in that folder and once you have tested and are ready to commit changes, copy and paste the files into
+the repository folder, replacing the old ones. Please make sure previously working code is not broken before you commit changes
+*/
 package FinalProject.ModellingThe3DWorld.codes280;
-
 
 import java.awt.BorderLayout;
 import java.awt.Color;
