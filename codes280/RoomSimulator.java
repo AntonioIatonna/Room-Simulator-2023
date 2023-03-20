b1b3e3a1350d0515aac64f9eb532d35bfd1d1f7e
Antonio Iatonna <ant.iatonna@gmail.com>
2023-03-20 16:33:09 -0400

    Added Mouse Click and Extra Textures

--- codes280/RoomSimulator.java
+++ codes280/RoomSimulator.java
@@ -2,7 +2,7 @@
 Make your changes in that folder and once you have tested and are ready to commit changes, copy and paste the files into
 the repository folder, replacing the old ones. Please make sure previously working code is not broken before you commit changes
 */
-package FinalProject.ModellingThe3DWorld.codes280;
+package codesAI280;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -11,6 +11,9 @@ import java.io.FileNotFoundException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import org.jogamp.java3d.*;
 import org.jogamp.java3d.loaders.IncorrectFormatException;
@@ -21,13 +24,17 @@ import org.jogamp.java3d.utils.geometry.Box;
 import org.jogamp.java3d.utils.geometry.Primitive;
 import org.jogamp.java3d.utils.image.TextureLoader;
 import org.jogamp.java3d.utils.universe.SimpleUniverse;
+import org.jogamp.java3d.utils.picking.PickResult;
+import org.jogamp.java3d.utils.picking.PickTool;
 import org.jogamp.vecmath.*;
 
-public class RoomSimulator extends JPanel {
+public class RoomSimulator extends JPanel implements MouseListener{
 
 	private static final long serialVersionUID = 1L;
 	private static JFrame frame;
-	private static String fileFormat = "FinalProject/ModellingThe3DWorld/codes280/"; // change this variable to whatever the file system requires on your computer
+	private Canvas3D canvas;
+	private static PickTool pickTool;
+	private static String fileFormat = "codesAI280/"; // change this variable to whatever the file system requires on your computer
     
 	private static TextureUnitState texState(String fn, TextureAttributes ta, TexCoordGeneration tcg) {
 		// Loads image:
@@ -114,15 +121,11 @@ public class RoomSimulator extends JPanel {
 		trsm_wall2.setTranslation(new Vector3f(-(x + depth), -(depth), 0.0f));
 		trsm_floor.setTranslation(new Vector3f(0.0f, -(y), 0.0f));
 
-		
-
 		// Add Textures:
-
 
 		TransformGroup floor = new TransformGroup();
 		floor.addChild(new Box(x, depth, z,Primitive.GENERATE_TEXTURE_COORDS, makeTexture("floor1.jpg")));
 		floor.setTransform(trsm_floor);
-		
 
 		TransformGroup wall1 = new TransformGroup();
 		wall1.addChild(new Box(x, y, depth,Primitive.GENERATE_TEXTURE_COORDS, makeTexture("wall1.jpg")));
@@ -131,10 +134,6 @@ public class RoomSimulator extends JPanel {
 		TransformGroup wall2 = new TransformGroup();
 		wall2.addChild(new Box(depth, y, z, Primitive.GENERATE_TEXTURE_COORDS, makeTexture("wall1.jpg")));
 		wall2.setTransform(trsm_wall2);
-
-
-
-
 
         roomTG.addChild(floor);
         roomTG.addChild(wall1);
@@ -154,6 +153,9 @@ public class RoomSimulator extends JPanel {
 		sceneBG.addChild(sceneTG);                         // keep the following stationary
 		sceneBG.addChild(Commons.add_Lights(Commons.White, 1));
 
+		pickTool = new PickTool( sceneBG );                // allow picking of objects in 'sceneBG'
+		pickTool.setMode(PickTool.GEOMETRY);                 // pick by bounding volume
+
 		return sceneBG;
 	}
 
@@ -161,6 +163,7 @@ public class RoomSimulator extends JPanel {
 	public RoomSimulator(BranchGroup sceneBG) {
 		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
 		Canvas3D canvas = new Canvas3D(config);
+		canvas.addMouseListener(this);                     // NOTE: enable mouse clicking
 		
 		SimpleUniverse su = new SimpleUniverse(canvas);    // create a SimpleUniverse
 		Commons.define_Viewer(su, new Point3d(15.00d, 10.0d, 15.0d));   // set the viewer's location
@@ -181,5 +184,44 @@ public class RoomSimulator extends JPanel {
 		frame.getContentPane().add(new RoomSimulator(create_Scene()));  // start the program
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent event) {
+		int x = event.getX(); int y = event.getY();        // mouse coordinates
+		Point3d point3d = new Point3d(), center = new Point3d();
+		canvas.getPixelLocationInImagePlate(x, y, point3d);// obtain AWT pixel in ImagePlate coordinates
+		canvas.getCenterEyeInImagePlate(center);           // obtain eye's position in IP coordinates
+		
+		Transform3D transform3D = new Transform3D();       // matrix to relate ImagePlate coordinates~
+		canvas.getImagePlateToVworld(transform3D);         // to Virtual World coordinates
+		transform3D.transform(point3d);                    // transform 'point3d' with 'transform3D'
+		transform3D.transform(center);                     // transform 'center' with 'transform3D'
+
+		Vector3d mouseVec = new Vector3d();
+		mouseVec.sub(point3d, center);
+		mouseVec.normalize();
+		pickTool.setShapeRay(point3d, mouseVec);           // send a PickRay for intersection
+
+		if (pickTool.pickClosest() != null) {
+            PickResult pickResult = pickTool.pickClosest();// obtain the closest hit
+            Shape3D clicked = (Shape3D) pickResult.getNode(PickResult.SHAPE3D);
+			System.out.println(clicked);
+            // Appearance app = new Appearance();             // originally a PRIMITIVE as a box
+            // if(clicked.equals(ring1.getShape3D())){
+			// 	RotationInterpolator R = (RotationInterpolator) sceneBG.getChild(3);
+			// 	if(! R.getAlpha().isPaused()){
+			// 		R.getAlpha().pause();
+			// 	}
+			// 	else{
+			// 		R.getAlpha().resume();
+			// 	}
+			// }
+        } 
+	}
+
+	public void mouseEntered(MouseEvent arg0) { }
+	public void mouseExited(MouseEvent arg0) { }
+	public void mousePressed(MouseEvent e) { }
+	public void mouseReleased(MouseEvent e) { }
 }
 
